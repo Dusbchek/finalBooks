5c94f870faa98e9a732686887c8d374f15fec5f1
Dusbchek <128949285+Dusbchek@users.noreply.github.com>
2023-11-16 14:21:43 -0600

    Update page.jsx

--- src/app/(admin)/dashboard/addbook/page.jsx
+++ src/app/(admin)/dashboard/addbook/page.jsx
@@ -48,8 +48,7 @@ export default function AddBook() {
                          <input type="tel" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mistery"  required/>
                           <input type="tel" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Drama"  required/>
                     </div>    
-                    </div>     
-                    </div>
+                    
                 </div>
                
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
